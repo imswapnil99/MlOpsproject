134bac0725a5771933dd69181c819e233fec730c
Swapnil khairnar <64456709+imswapnil99@users.noreply.github.com>
2020-05-30 16:41:08 +0530

    proper syntax

--- mail.py
+++ mail.py
@@ -4,10 +4,10 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("swapnilkhairnar.78@gmail.com", "SHkhairnar1178") 
+s.login("sender mail id", "password") 
 # message to be sent 
 message = "Hey Developer, you make it and IT WORKS. "
 # sending the mail 
-s.sendmail("swapnilkhairnar.78@gmail.com", "swapnilkhairnar.78@gmail.com", message) 
+s.sendmail("sender mail id", "recevier mail id", message) 
 # terminating the session 
 s.quit()
