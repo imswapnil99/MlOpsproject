592a281f8f56754ad4265e5fcd851d130f1bb3d3
Swapnil Khairnar <swapnilkhairnar.78@gmail.com>
2020-05-30 13:55:41 +0530

    mail comit1

--- mail.py
+++ mail.py
@@ -6,7 +6,7 @@ s.starttls()
 # Authentication 
 s.login("swapnilkhairnar.78@gmail.com", "SHkhairnar1178") 
 # message to be sent 
-message = "Hey Developer, Finally we got the model trained. "
+message = "Hey Developer, you make it and IT WORKS. "
 # sending the mail 
 s.sendmail("swapnilkhairnar.78@gmail.com", "swapnilkhairnar.78@gmail.com", message) 
 # terminating the session 
